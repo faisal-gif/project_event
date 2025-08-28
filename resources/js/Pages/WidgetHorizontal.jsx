9e06dab7ae816a3d4c206bb59bc8c391089267bd
faisal-gif <fagdera@gmail.com>
2025-08-28 15:36:53 +0700

    modified:   resources/js/Pages/WidgetHorizontal.jsx

--- resources/js/Pages/WidgetHorizontal.jsx
+++ resources/js/Pages/WidgetHorizontal.jsx
@@ -25,15 +25,15 @@ function WidgetHorizontal({ events }) {
         <>
             <Head title="Widget Event" />
 
-            <div className='flex flex-col justify-center items-center gap-4'>
-                <h1 className='text-2xl lg:text-4xl font-bold'>Bosan dengan rutinitas?</h1>
-                <h4 className='text-sm lg:text-2xl'>Saatnya Pilih Event Seru yang Cocok untukmu!</h4>
+            <div className=' flex flex-col justify-center items-center gap-4'>
+                <h2 className='text-2xl lg:text-4xl font-bold text-[#b51d1d]'>Jenuh dengan kesibukan?</h2>
+                <h4 className='text-sm lg:text-2xl text-center'>Mulai akhir pekanmu dengan seru, pilih event favoritmu sekarang!</h4>
                 <Carousel opts={{ align: "start", loop: true }} className="w-full" plugins={[Autoplay()]}>
                     <Carousel.Content className="-ml-4">
                         {events.map((event) => (
                             <Carousel.Item
                                 key={event.id}
-                                className="pl-4 min-w-0 shrink-0 grow-0 basis-full sm:basis-1/3 lg:basis-1/4"
+                                className="pl-4 min-w-0 shrink-0 grow-0 basis-9/12 lg:basis-1/4"
                             >
                                 <div className="p-2 h-full">
                                     <div key={event.id} className="card bg-base-100 shadow-xl hover:shadow-2xl transition-shadow">
@@ -63,14 +63,14 @@ function WidgetHorizontal({ events }) {
 
                                             <h2 className="card-title text-sm mb-2 text-base-content">{event.title}</h2>
 
-                                            <div className="card-actions justify-between items-center">
+                                            <div className="card-actions flex flex-col md:flex-row  justify-between ">
                                                 <div className="text-xl font-bold text-primary">
                                                     {formatPrice(event.price)}
                                                 </div>
                                                 <a
                                                 target='_blank'
                                                     href={route('events.guest.detail', event)}
-                                                    className="btn btn-primary btn-sm"
+                                                    className="btn btn-primary btn-sm w-full md:w-auto"
                                                 >
                                                     Lihat Detail
                                                 </a>
@@ -82,6 +82,7 @@ function WidgetHorizontal({ events }) {
                         ))}
                     </Carousel.Content>
                 </Carousel>
+                
             </div>
         </>
     )
