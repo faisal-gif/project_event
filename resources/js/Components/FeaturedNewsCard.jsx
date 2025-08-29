55ca9bf3be083d32b65a7d35c4368aee10fe5595
faisal-gif <fagdera@gmail.com>
2025-08-29 19:59:23 +0700

    Refactor FeaturedNewsCard component to improve link formatting and update remaining quota display logic

--- resources/js/Components/FeaturedNewsCard.jsx
+++ resources/js/Components/FeaturedNewsCard.jsx
@@ -26,7 +26,7 @@ function FeaturedNewsCard({
         });
     }
     return (
-        <Link  href={route('events.guest.detail', event)}>
+        <Link href={route('events.guest.detail', event)}>
             <Card className={` group relative h-60 w-full cursor-pointer overflow-hidden rounded-lg border-0 shadow-lg transition-all duration-300 hover:shadow-2xl lg:h-[28rem] ${className}`}>
                 <div
                     className="absolute inset-0 bg-cover bg-center transition-transform duration-500 ease-in-out group-hover:scale-110"
@@ -58,17 +58,16 @@ function FeaturedNewsCard({
                             </div>
                         )}
 
-                        <div className="flex items-center text-sm text-white" >
+                        <div className="flex items-center text-sm text-white">
                             <svg className="w-4 h-4 mr-2" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                                 <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M17 20h5v-2a3 3 0 00-5.356-1.857M17 20H7m10 0v-2c0-.656-.126-1.283-.356-1.857M7 20H2v-2a3 3 0 015.356-1.857M7 20v-2c0-.656.126-1.283.356-1.857m0 0a5.002 5.002 0 019.288 0M15 7a3 3 0 11-6 0 3 3 0 016 0zm6 3a2 2 0 11-4 0 2 2 0 014 0zM7 10a2 2 0 11-4 0 2 2 0 014 0z"></path>
                             </svg>
-                            {event.remainingQuota} / {event.quota} tersisa
+                            {event.remainingQuota === 0
+                                ? 'Sold Out'
+                                : `${event.remainingQuota} / ${event.quota} tersisa`}
                         </div>
                     </div>
-
-
                 </div>
-
             </Card>
         </Link>
     )
