ca76612cd32cedb7db406ba9d00fa812d019cde3
faisal-gif <fagdera@gmail.com>
2025-10-17 17:15:37 +0700

    inertia ssr

--- app/Http/Middleware/HandleInertiaRequests.php
+++ app/Http/Middleware/HandleInertiaRequests.php
@@ -3,6 +3,7 @@
 namespace App\Http\Middleware;
 
 use Illuminate\Http\Request;
+use Inertia\Inertia;
 use Inertia\Middleware;
 
 class HandleInertiaRequests extends Middleware
@@ -31,6 +32,7 @@ class HandleInertiaRequests extends Middleware
     {
         return [
             ...parent::share($request),
+            'ssr' => fn () => Inertia::getShared('ssr'),
             'auth' => [
                 'user' => $request->user(),
             ],
