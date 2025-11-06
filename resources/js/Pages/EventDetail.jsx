1de5d1948cc8c2f6c756143ef8a768bcbecf9619
faisal-gif <fagdera@gmail.com>
2025-11-06 12:41:51 +0700

    modified:   resources/js/Pages/EventDetail.jsx

--- resources/js/Pages/EventDetail.jsx
+++ resources/js/Pages/EventDetail.jsx
@@ -148,7 +148,7 @@ function EventDetail({ event, seo }) {
                                     </div>
                                     {event.location_details && (
                                         <div className="flex items-center gap-3 text-muted-foreground">
-                                            <MapPin className="h-5 w-5 text-primary" />
+                                            <MapPin className="h-8 w-8 text-primary" />
                                             <span className="text-lg">{event.location_details}</span>
                                         </div>
                                     )}
