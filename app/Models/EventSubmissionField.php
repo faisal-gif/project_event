0d1895a9825b59901fbc0c855efdcacff7b81fb6
faisal-gif <fagdera@gmail.com>
2026-02-09 14:59:54 +0700

    modified:   app/Models/EventSubmissionField.php

--- app/Models/EventSubmissionField.php
+++ app/Models/EventSubmissionField.php
@@ -8,4 +8,10 @@ class EventSubmissionField extends Model
 {
     protected $table = 'event_submission_fields';
     protected $guarded = [];
+
+
+    protected $casts = [
+        'options' => 'array',        // Ini kuncinya!
+        'is_required' => 'boolean',
+    ];
 }
